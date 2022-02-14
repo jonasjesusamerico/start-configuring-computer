c2b8643bcde3243bd2c7e0a9e88e70e5c2e87826
Jonas de Jesus Américo <jjsamerico@gmail.com>
2022-02-13 22:05:39 -0300

    Update post-installation.sh
    Atualizando lib

--- post-installation/program/post-installation.sh
+++ post-installation/program/post-installation.sh
@@ -70,7 +70,7 @@ echo 'Returning the root folder...'
 cd ~
 
 echo 'Installing nvm...' 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
